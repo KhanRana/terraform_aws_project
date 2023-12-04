7f16207ad3d962608e6620cb5de7f1a171bc6c11
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-12-04 22:34:14 +0000

    refactor security module to allow ssh connection

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,14 +1,54 @@
-# Get the IP for ssh
+# Get the IP
 data "http" "myipaddr" {
    url = "http://icanhazip.com"
 }
 
-resource "aws_security_group" "ssh" {
-  vpc_id = var.vpc_id
+# create security group
+resource "aws_security_group" "smart_home" {
+  name        = "smart_home"
+  description = "Security group for app servers"
+  vpc_id      = var.vpc_id
+}
+
+# add rules to the security group
+resource "aws_vpc_security_group_ingress_rule" "http_ipv6" {
+  security_group_id = aws_security_group.smart_home.id
+
+  cidr_ipv6   = "::/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_ipv4" {
+  security_group_id = aws_security_group.smart_home.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_port_ipv6" {
+  security_group_id = aws_security_group.smart_home.id
+
+  cidr_ipv6   = "::/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_port_ipv4" {
+  security_group_id = aws_security_group.smart_home.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
-  security_group_id = aws_security_group.ssh.id
+  security_group_id = aws_security_group.smart_home.id
 
   cidr_ipv4   = "${chomp(data.http.myipaddr.response_body)}/32"
   from_port   = 22
@@ -16,41 +56,16 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   to_port     = 22
 }
 
-# create security group
-resource "aws_security_group" "ingress_control" {
-  vpc_id = var.vpc_id
+resource "aws_vpc_security_group_egress_rule" "outgoing_ipv6" {
+  security_group_id = aws_security_group.smart_home.id
+
+  cidr_ipv6   = "::/0"
+  ip_protocol = "-1"
 }
 
-resource "aws_security_group_rule" "allow_http_ingress" {
-  type = "ingress"
-  security_group_id = aws_security_group.ingress_control.id
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
-}
+resource "aws_vpc_security_group_egress_rule" "outgoing_ipv4" {
+  security_group_id = aws_security_group.smart_home.id
 
-resource "aws_security_group_rule" "allow_https_ingress" {
-  type = "ingress"
-  security_group_id = aws_security_group.ingress_control.id
-  from_port = "443"
-  to_port = "443"
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
-}
-
-resource "aws_security_group" "egress_control" {
-  vpc_id = var.vpc_id
-}
-
-resource "aws_security_group_rule" "allow_egress" {
-  type = "egress"
-  security_group_id = aws_security_group.egress_control.id
-  from_port = "0"
-  to_port = "0"
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }

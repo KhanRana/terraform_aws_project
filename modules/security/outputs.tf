e919e33538f3daa05756f989f001276b133a02cd
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-12-06 14:28:33 +0000

    create dynamo db table

--- modules/security/outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "security_group_ids" {
-  value = aws_security_group.smart_home.id
-}
--- modules/smart_home_server/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output the list of all instances ids
-output "instance_id" {
-  value = aws_instance.smart_home[*].id
-}
-
-# output the number of ec2 servers created 
-output "no_app_ec2" {
-  value = length(aws_instance.smart_home[*])
-}

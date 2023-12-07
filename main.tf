b53524dc7d269d5cba3729a22ff42ff6c6e77ba6
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-12-07 23:45:07 +0000

    create autoscaled instances for each availability zone

--- main.tf
+++ main.tf
@@ -45,3 +45,14 @@ module "load_balancing" {
   subnets         = module.vpc.public_subnets
   apps = ["lights", "heating", "status"]
 }
+
+# Create autoscaling groups one instance available in each region 
+module "autoscaling" {
+  count = length(module.load_balancing.lb-tg-arn)
+  source = "./modules/autoscaling"
+  AMI = ["ami-08c766676481fd5aa", "ami-08aeb32d2c5d96e18", "ami-0bb05632bcd4998a5"]
+  security_groups = module.security.security_group_ids
+  public_subnets = module.vpc.public_subnets
+  instance_type = "t2.micro"
+  lb-tg-arn = module.load_balancing.lb-tg-arn[count.index]
+}
